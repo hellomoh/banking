91f033df6375ea74dad53d8393cbfbaac12d0421
hellomoh <mohitguptawork1@gmail.com>
2024-05-28 15:53:11 +0530

    Implement auth, appwrite and sentry

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/sheet"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
+import Footer from "./Footer"
 
 
 const MobileNav = ({ user }: MobileNavProps) => {
@@ -73,7 +74,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
             USER
             </nav>
           </SheetClose>
-          FOOTER
+          <Footer user={user} type="mobile" />
         </div>
           
         </SheetContent>
